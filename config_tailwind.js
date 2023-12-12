70e9801788922831442622fb92d9e8b66e23f4f1
akhil <gollaakhil1115@gmail.com>
2023-12-11 19:15:53 -0600

    adding tailwind basics

---
+++ config_tailwind.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    theme: {
+      extend: {
+        colors: {
+          chestnut: '#973F29',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
